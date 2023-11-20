6d400148e7c43f88e4be300407794ef964aaf6a8
Messi Lee <hojunlee@wustl.edu>
2023-11-19 21:18:42 -0600

    Re-organize Tables Folder
    Place all relevant files inside the Tables folder

--- Tables/tableS2.R
+++ Tables/tableS2.R
@@ -1,10 +1,11 @@
 
 ## Anonymous
-# The Effect of Group Status on the Variability of Group Representations in LLM-generated Text
+# Large Language Models Portray Socially Subordinate Groups as More Homogeneous, 
+# Consistent with a Bias Observed in Humans
 
-## Script date: 29 Oct 2023
+## Script date: 19 Nov 2023
 
-# Install and/or Load Packages -------------------------------------------------
+# Install and/or load packages -------------------------------------------------
 
 if(!require("tidyverse")){install.packages("tidyverse", dependencies = TRUE); require("tidyverse")}
 if(!require("text")){install.packages("text", dependencies = TRUE); require("text")}
@@ -13,13 +14,13 @@ if(!require("effectsize")){install.packages("effectsize", dependencies = TRUE); 
 if(!require("effsize")){install.packages("effsize", dependencies = TRUE); require("effsize")}
 if(!require("ggsci")){install.packages("ggsci", dependencies = TRUE); require("ggsci")}
 
-# Initialize the text Package ---------------------------------------------------
+# Initialize the text package ---------------------------------------------------
 
 # textrpp_install()
 # textrpp_initialize(save_profile = TRUE)
 # ?textrpp_initialize
 
-# Define Function to Perform Preprocessing -------------------------------------
+# Define function to perform preprocessing -------------------------------------
 
 original_prep = function(x) {
   x = str_to_lower(x) # make text lower case
@@ -27,26 +28,26 @@ original_prep = function(x) {
   x = str_replace_all(x, "\\s+", " ") # collapse multiple spaces
 }
 
-# Load Stories -----------------------------------------------------------------
+# Load generated text ----------------------------------------------------------
 
 setwd('Pilot Data')
 
-stories <- read.csv('pilot_data.csv') %>%
+data <- read.csv('pilot_data.csv') %>%
   mutate(clean_text = original_prep(text)) %>%
   mutate(clean_text = gsub('"', '', clean_text)) %>%
   mutate(clean_text = gsub(',', '', clean_text))
 
 # Sample three stories that are coded as 1 and three coded as 0
-sampled_stories <- stories %>% group_by(hardship) %>% sample_n(3)
+sampled_data <- data %>% group_by(hardship) %>% sample_n(3)
 
-# Separate Stories by Gender and Racial/Ethnic Group ---------------------------
+# Separate texts by gender and race/ethnicity ----------------------------------
 
-black <- stories %>% filter(race == "African")
-asian <- stories %>% filter(race == "Asian")
-hispanic <- stories %>% filter(race == "Hispanic")
-white <- stories %>% filter(race == "White")
+black <- data %>% filter(race == "African")
+asian <- data %>% filter(race == "Asian")
+hispanic <- data %>% filter(race == "Hispanic")
+white <- data %>% filter(race == "White")
 
-# Create Sentence Embeddings for the Generated Stories -------------------------
+# Create sentence embeddings for the generated texts ---------------------------
 
 black_embeddings = textEmbed(black$text, keep_token_embeddings = FALSE)
 black_embeds <- as.matrix(black_embeddings[['texts']]$text)
@@ -60,7 +61,7 @@ hispanic_embeds <- as.matrix(hispanic_embeddings[['texts']]$text)
 white_embeddings = textEmbed(white$text, keep_token_embeddings = FALSE)
 white_embeds <- as.matrix(white_embeddings[['texts']]$text)
 
-# Calculate Similarity between Sentence Embeddings within Groups ---------------
+# Calculate similarity between sentence embeddings within groups ---------------
 
 black_cosines <- sim2(black_embeds)
 bmc <- black_cosines[upper.tri(black_cosines)]
@@ -74,7 +75,7 @@ hmc <- hispanic_cosines[upper.tri(hispanic_cosines)]
 white_cosines <- sim2(white_embeds)
 wmc <- white_cosines[upper.tri(white_cosines)]
 
-# Calculate the Mean -----------------------------------------------------------
+# Calculate the mean -----------------------------------------------------------
 
 black_mean <- mean(bmc)
 asian_mean <- mean(amc)
